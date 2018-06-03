104de157f58b3f50c998c692bab53af7618eec51
faisalkhalidsi <faisal.khalid.si@gmail.com>
2018-06-03 23:30:45 +0700

    Event Detail Changed

--- views/event/_detail.php
+++ views/event/_detail.php
@@ -22,24 +22,32 @@ GridView::widget([
     'dataProvider' => $dataProvider,
     'columns' => [
         ['class' => 'yii\grid\SerialColumn'],
-        'title',
+        [
+            'label' => 'Title',
+            'format' => 'raw',
+            'value' => function ($dataProvider) {
+//                return Html::a($dataProvider->title, ['/event/view', 'id' => $dataProvider->id], ['target' => '_blank']);
+                return Html::a($dataProvider->title, ['/event/view', 'id' => $dataProvider->id], ['target' => '_blank', 'data-pjax' => "0"]);
+            },
+        ],
+//        'title',
         'category',
-        'description',
+//        'description',
         'created_date',
 //        'end_date',
-        ['class' => 'yii\grid\ActionColumn',
-            'template' => '{view}',
-            'headerOptions' => ['class' => 'activity-view-link',],
-            'contentOptions' => ['class' => 'padding-left-5px'],
-            'buttons' => [
-                'view' => function ($url, $model, $key) {
-                    return Html::a('<span class="glyphicon glyphicon-search"></span>', '#', [
-                                'class' => 'activity-view-link',
-                                'data-pjax' => "0",
-                    ]);
-                },
-            ],
-        ],
+//        ['class' => 'yii\grid\ActionColumn',
+//            'template' => '{view}',
+//            'headerOptions' => ['class' => 'activity-view-link',],
+//            'contentOptions' => ['class' => 'padding-left-5px'],
+//            'buttons' => [
+//                'view' => function ($url, $model, $key) {
+//                    return Html::a('<span class="glyphicon glyphicon-search"></span>', '#', [
+//                                'class' => 'activity-view-link',
+//                                'data-pjax' => "0",
+//                    ]);
+//                },
+//            ],
+//        ],
     ],
 ]);
 ?>
